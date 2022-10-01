ca30786ff84709e094f45e1ff6134575b7705be2
Gustavo Schimite Soares <gustavoschimitesoares0211@gmail.com>
2022-09-30 22:03:57 -0300

    [ATIVIDADE CADASTROS] Aula 2 de BD

--- BancoDados/Aula02/SQL_INSERT_pessoa.sql
+++ BancoDados/Aula02/SQL_INSERT_pessoa.sql
@@ -1,6 +1,10 @@
-INSERT INTO pessoa (nome,
+INSERT INTO pessoa (id,
+					nome,
                     cgccpf,
                     tipo_pessoa)
-VALUES ('Romero Grito', '101-999-999-11', 'PF'),
-        ('Lucio Estava', '567-765-666-77', 'PF')
+VALUES (1, 'Gustavo Schimite Soares', '101-999-444-11', 'PF'),
+		(2, 'Patrick Weber', '141-999-999-11', 'PJ'),
+		(3, 'Yasmim Alves', '101-777-676-11', 'PF'),
+		(4, 'Romero Grito', '101-999-999-11', 'PF'),
+        (5, 'Lucio Estava', '567-765-666-77', 'PF')
 ;
